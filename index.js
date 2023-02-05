3b2037e56ae46dc5a5a93a9adc231897b39f17d0
stutisk <48529714+stutisk@users.noreply.github.com>
2023-02-05 16:54:02 +0530

    changes

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 var Button = document.querySelector(".submitBtn");
 var Name = document.querySelector(".form-input-name");
+var Email = document.querySelector(".form-input-email")
 Button.disabled = true;
 
 Button.addEventListener("click", () => {
   console.log("hey");
 });
 
-Name.addEventListener("input", (e) => {
+// Name.addEventListener("input", (e) => {
 
 
-  if (e.target.value.length < 1  ) {
-  console.log("enter value");
-  Button.disabled = true;
+//   if (e.target.value.length < 1  ) {
+//   console.log("enter value");
+//   Button.disabled = true;
   
-  } else {
-    console.log(e.target.value.length);
-    Button.disabled = false;
+//   } else {
+//     console.log(e.target.value.length);
+//     Button.disabled = false;
 
-  }
-});
+//   }
+// });
+
+
+Name.addEventListener("keyup",see)
+Email.addEventListener("keyup",see)
+
+
+
+function see(e) {
+  if (e.target.value.length < 10 ) {
+    console.log(e.target.value)
+    console.log("enter value");
+    Button.disabled = true;
+    
+    } else {
+      console.log(e.target.value);
+      Button.disabled = false;
+  
+    }
+}
